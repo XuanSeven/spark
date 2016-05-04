ec7f5facb41f284914b403c77385c23127e64c63
Ling Yuan <lyuan@comprehend.com>
2016-05-03 17:47:21 -0700

    adding hive deps

--- build.sbt
+++ build.sbt
@@ -7,16 +7,19 @@ scalaVersion := "2.11.7"
 libraryDependencies ++= Seq(
   "org.apache.spark"              %%  "spark-core"              % "1.6.0",
   "org.apache.spark"              %%  "spark-sql"               % "1.6.0",
-  "org.postgresql"                %   "postgresql"              % "9.4-1201-jdbc41"
+  "org.apache.spark"              %%  "spark-hive"              % "1.6.0",
+  "org.postgresql"                %   "postgresql"              % "9.3-1104-jdbc41"
 )
 
 assemblyMergeStrategy in assembly <<= (mergeStrategy in assembly) { (old) =>
 {
-  case PathList("javax", "servlet", xs @ _*) => MergeStrategy.last
-  case PathList("javax", "activation", xs @ _*) => MergeStrategy.last
-  case PathList("org", "apache", xs @ _*) => MergeStrategy.last
-  case PathList("com", "google", xs @ _*) => MergeStrategy.last
-  case PathList("com", "esotericsoftware", xs @ _*) => MergeStrategy.last
-  case PathList("com", "codahale", xs @ _*) => MergeStrategy.last
-  case x => old(x)
-}}
+  case m if m.toLowerCase.endsWith("manifest.mf") => MergeStrategy.discard
+  case m if m.startsWith("META-INF") => MergeStrategy.discard
+  case PathList("javax", "servlet", xs @ _*) => MergeStrategy.first
+  case PathList("org", "apache", xs @ _*) => MergeStrategy.first
+  case PathList("org", "jboss", xs @ _*) => MergeStrategy.first
+  case "about.html"  => MergeStrategy.rename
+  case "reference.conf" => MergeStrategy.concat
+  case _ => MergeStrategy.first
+}
+}
--- src/main/scala/com/comprehend/sparketl/SparkETL.scala
+++ src/main/scala/com/comprehend/sparketl/SparkETL.scala
@@ -2,23 +2,69 @@ package com.comprehend.sparketl
 
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.hive.HiveContext
 
 object SparkETL {
+
+  val sql1 =
+    """
+      |select
+      |		RoleID,
+      |		RolePermissions,
+      |		Active,
+      |		Created,
+      |		Updated,
+      |		ServerSyncDate,
+      |		ViewAllSites,
+      |		CreateSubjects,
+      |		ModifyPrimaryRecord,
+      |		IsInvestigator,
+      |		SubjectAdmin,
+      |		SeeAudits,
+      |		ChangeInvestigator,
+      |		ShareSubject,
+      |		CanViewStudyGrid,
+      |		CanBatchSign,
+      |		CanInactivateForm,
+      |		CanReactivateForm,
+      |		UsePrimarySubjectName,
+      |		CanUseTemplates,
+      |		CanModifyTemplates,
+      |		SetClinicalSignificance,
+      |		UseLabMaintenance,
+      |		ViewAllSubjects,
+      |		Pass1DataEntry,
+      |		Pass2DataEntry,
+      |		Reconcile,
+      |		RoleNameID,
+      |		CanUseAdvancedDCFs,
+      |		CanUseDCFs
+      |from RolesAllModules
+    """.stripMargin
+
   def main(args: Array[String]) {
     val conf = new SparkConf()
     val sc = new SparkContext(conf)
-    val sqc = new SQLContext(sc)
+    val hc = new HiveContext(sc)
+    hc.setConf("", "")
 
     println("hello world")
 
-    val foo =  sqc.read.format("jdbc")
-      .option("url", "jdbc:postgresql://god1-postgres.sf.comprehend.com:5432/xli")
+    val foo = hc.read.format("jdbc")
+      .option("url", "jdbc:postgresql://dal-metal-edwards-1.dallas.comprehend.com:5476/edwards-dev-clinical")
       .option("driver", "org.postgresql.Driver")
-      .option("user", "comprehend")
-      .option("password", "office2005")
-      .option("dbtable", "spark.testtable")
+      .option("user", "edwards-dev")
+      .option("password", "KYwhdeNCr9u34KZwd2uyUeB")
+      .option("dbtable", "dbo.rolesallmodules")
       .load()
+    println("load finished")
+    foo.registerTempTable("rolesallmodules")
+    println("registered temptable")
 
-    foo.show()
+    hc.sql(sql1)
+
+    val df = hc.sql("select * from dbo.roles")
+
+    df.show()
   }
 }
